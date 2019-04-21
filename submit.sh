b38010a7927f5e7a566441871ca009cea20fe49f
Markus Tran <thebluepillow@gmail.com>
2019-04-21 13:54:10 -0700

    test submit

---
+++ submit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ "$1" == "" ]; then
+  echo "No folder selected"
+  exit
+fi
+
+DIR=${@%/}
+
+# Create a new branch with only the folder specified
+git checkout -B $DIR
+git rm -rf .
+git checkout HEAD -- $DIR
+
+# Move all the files to the root of the repository
+mv $DIR/* .
+git rm -rf $DIR
+
+# Commit everything
+git add -A
+git commit -m "Submit $DIR"
+
+# Push and move back to master
+git push -u origin $DIR
+git checkout master
