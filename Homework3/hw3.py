acfe3fc4e95db8b0a26736f02e2850c3bcc4c5db
Markus Tran <thebluepillow@gmail.com>
2019-04-26 10:16:26 -0700

    hw3 files

---
+++ Homework3/hw3.py
@@ -0,0 +1,21 @@
+# Your name and email here
+
+# Problem 1
+
+def findSmallest(items):
+    raise NotImplementedError
+
+# Problem 2
+
+def findValue(value, linkedList):
+    raise NotImplementedError
+
+# Problems 3 and 5
+
+def ladder(rungs):
+    raise NotImplementedError
+
+# Problem 4
+
+def recPal(str):
+    raise NotImplementedError
