a20cf1b1367f02e3f4d225cdd3378c55ffb3c6ef
Markus Tran <thebluepillow@gmail.com>
2019-04-26 10:25:42 -0700

    hw3 p1

--- Homework3/hw3.py
+++ Homework3/hw3.py
@@ -1,9 +1,22 @@
 # Your name and email here
+# Markus Tran
+# mkhtran@ucdavis.edu
 
 # Problem 1
 
 def findSmallest(items):
-    raise NotImplementedError
+    length = len(items)
+
+    # Base cases
+    if (length == 0):
+        return None
+    if (length == 1):
+        return items[0]
+
+    # Return the first element OR the smallest of the remaining elements
+    first = items[0]
+    nextSmallest = findSmallest(items[1:])
+    return first if first <= nextSmallest else nextSmallest
 
 # Problem 2
 
